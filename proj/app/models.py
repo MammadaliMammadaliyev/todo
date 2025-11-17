8e8b85b57a9912955b88f0359a350dbea903c1ac
MammadaliMammadaliyev-CHI <mammadali.mammadaliyev@chisw.com>
2025-11-17 21:20:14 +0200

    created new app & first model

---
+++ proj/app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Task(models.Model):
+    user = models.ForeignKey(
+        User,   
+        on_delete=models.CASCADE, 
+        related_name="tasks",
+        null=True,
+        blank=True
+    )
+    title = models.CharField(max_length=200)
+    deadline = models.DateTimeField(null=True, blank=True)    
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
