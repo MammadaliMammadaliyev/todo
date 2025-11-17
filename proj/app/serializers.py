67b2842bc0fd86ba3ad32765ee0f35b53e3b21c5
MammadaliMammadaliyev-CHI <mammadali.mammadaliyev@chisw.com>
2025-11-17 21:27:21 +0200

    first views, serializers, urls config (rest apis)

---
+++ proj/app/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Task
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Task
+        fields = [
+            "id", 
+            "user", 
+            "title", 
+            "deadline", 
+            "is_completed"
+        ]
--- proj/app/views.py
+++ proj/app/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+
+class TaskAPIView(APIView):
+    def get(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
